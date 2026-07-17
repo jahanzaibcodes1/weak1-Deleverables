0136c1dea308143acbc3b756e47a6813b2148483
jahanzaibcodes1 <jzaib7826@gmail.com>
2026-07-17 13:22:41 +0500

    feat: add math object practice

---
+++ Day-2/mathobject.js
@@ -0,0 +1,32 @@
+//Practice of Math object// Math Object Practice
+
+console.log("PI:", Math.PI);
+console.log("Euler Number:", Math.E);
+
+let num = 4.7;
+
+console.log("Round:", Math.round(num));
+console.log("Floor:", Math.floor(num));
+console.log("Ceil:", Math.ceil(num));
+console.log("Truncate:", Math.trunc(num));
+
+console.log("Absolute:", Math.abs(-15));
+
+console.log("Power:", Math.pow(2, 5));
+console.log("Square Root:", Math.sqrt(64));
+console.log("Cube Root:", Math.cbrt(27));
+
+console.log("Maximum:", Math.max(10, 20, 30, 40));
+console.log("Minimum:", Math.min(10, 20, 30, 40));
+
+console.log("Random:", Math.random());
+console.log("Random 1-10:", Math.floor(Math.random() * 10) + 1);
+
+console.log("Sin 90°:", Math.sin(Math.PI / 2));
+console.log("Cos 0°:", Math.cos(0));
+
+console.log("Log:", Math.log(10));
+console.log("Log10:", Math.log10(100));
+
+console.log("2^3:", Math.pow(2, 3));
+console.log("5 squared:", 5 ** 2);
